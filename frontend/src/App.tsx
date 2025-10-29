ed30fe14531edfe9bbc1da8cfeba35b1009a2df7
Nguyen Ho Thanh Minh <nghothminz@gmail.com>
2025-10-29 17:20:48 +0700

    Code Header UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css';
 import Footer from './components/layout/Footer';
+import Header from './components/layout/Header';
 
 function App() {
   return (
     <div>
-      <Footer />
+      <Header />
     </div>
   );
 }
